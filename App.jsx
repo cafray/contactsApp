c683c326116d5dcef5829f9f9b4c66c6fecfaa76
Fernando Varela <fernandoandesvarela@gmail.com>
2023-03-07 03:24:58 +0200

    Converted to TypeScript

--- App.jsx
+++ App.jsx
@@ -2,10 +2,8 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import PaginaLogin from './telas/PaginaLogin';
 import PaginaInicial from './telas/PaginaInicial';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import OneDrawer from './componentes/OneDrawer';
 import { OneProvider } from './servicos/OneContexto';
-import OneStack from './componentes/OneStack';
 
 //const Drawer = createDrawerNavigator();
 
